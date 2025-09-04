f8a90e469553fac9f22240b8413ca8db7335b719
Rekhansh Raje Deshmukh <rekhanshdeshmukh@gmail.com>
2025-09-04 12:36:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
     name="Generative AI project",
     version="0.0.0",
-    author="Aditya,Fateh,Aryan,Khushi,Rekhansh",
+    author="Rekhansh",
     author_email="forcoding70@gmail.com",
     packages=find_packages(),
     install_requires=[
@@ -27,7 +27,7 @@ from setuptools import find_packages, setup
 setup(
     name="Generative AI project",
     version="0.0.0",
-    author="Aditya,Fateh,Aryan,Khushi,Rekhansh",
+    author="Rekhansh",
     author_email="forcoding70@gmail.com",
     packages=find_packages(),
     install_requires=[
